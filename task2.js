751e566a2f987440b9426547f95f54d88e1c69c6
Aplake <aplake@mail.ru>
2018-06-07 11:25:49 +0300

    Изменен алгоритм сдвига массива, но не найден миниманльный

--- task2.js
+++ task2.js
@@ -3,21 +3,30 @@ var appPath = process.argv[1];
 var testStr_1 = process.argv[2];
 var testStr_2 = process.argv[3];
 var counter = 0;
+var not_equal = false;
 
 var myArray_1 = testStr_1.split('');
 var myArray_2 = testStr_2.split('');
-console.log(myArray_1);
-console.log(myArray_2);
 
 for(var i = 0; i<myArray_1.length; i++){
-    if(myArray_1[i] === myArray_2[i]){
-        counter = -1;
-        break;     
+    if(myArray_1[i] != myArray_2[i]){
+        not_equal = true;
+        counter = 0;
+        break;
     }else{
-        myArray_2=myArray_2.splice(-1).concat(myArray_2);
-        counter = counter + 1;
-        if(myArray_1[i] === myArray_2[i]){
-            break; 
+        counter = -1;
+    }
+}
+
+while(not_equal){
+    myArray_1=myArray_1.splice(-1).concat(myArray_1);
+    counter +=1;
+    for(var i = 0; i<myArray_1.length; i++){
+        if(myArray_1[i] != myArray_2[i]){
+            not_equal = true;
+            continue;
+        }else{
+            not_equal = false;
         }
     }
 }
