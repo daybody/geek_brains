9327da8a25c764ee8515af2cc147d2a74610bc3c
Aplake <aplake@mail.ru>
2018-06-13 22:41:39 +0300

    Правильный поиск минимального значение

--- task2.js
+++ task2.js
@@ -1,60 +1,98 @@
-/* 
-    Добавить сдвиги влево вправо через pop/push и shift/unshift +
-    Проверка на существание сдвига : если за n = array.lenght итераций совпадение масив не стал равным то -1
-    Если масивы равны то counter = 0 +
-*/
-
-var nodePath = process.argv[0];
+﻿var nodePath = process.argv[0];
 var appPath = process.argv[1];
-var testStr_1 = process.argv[2];
-var testStr_2 = process.argv[3];
-var counter = 0;
+var str_1 = process.argv[2];
+var str_2 = process.argv[3];
+var not_equal_left = false;
+var not_equal_right = false;
+var counter = -2;
+var num_equal = 0;
+var arr_l = [];
+var arr_r = [];
 var counter_l = 0;
 var counter_r = 0;
-var not_equal = false;
 
+arr_1 = str_1.split('');
+arr_2 = str_2.split('');
 
-var Arr_1 = testStr_1.split('');
-var Arr_2 = testStr_2.split('');
+len_1 = arr_1.length;
+len_2 = arr_2.length;
 
-for(var i = 0; i<Arr_1.length; i++){
-    if(Arr_1[i] != Arr_2[i]){
-        not_equal = true;
+if (len_1 != len_2){
+    counter = -1;
+}
+
+if (counter != -1){
+    for(var i = 0; i < len_1; i++){
+        if (arr_1[i] == arr_2[i]){
+            num_equal += 1;
+        }
+    }
+
+    if(num_equal != len_1){
+        not_equal_left = true;
+        arr_l = arr_1.concat();
+    
+        not_equal_right = true;
+        arr_r = arr_1.concat();
+    }else{
+        counter = 0; 
+    }
+}
+
+while(not_equal_left){
+    arr_l = arr_l.splice(1).concat(arr_l);
+    counter_l += 1;
+    num_equal = 0;
+
+    for(var i = 0; i < len_1; i++){
+        if (arr_l[i] == arr_2[i]){
+            num_equal += 1;
+        }
+    }
+   
+    if (num_equal == len_1){
+        not_equal_left = false;
+    }
+
+    if(counter_l == len_1){
+        counter = -1;
         break;
     }
 }
-console.log(Arr_1);
 
+while(not_equal_right){
+    arr_r = arr_r.splice(-1).concat(arr_r);
+    counter_r += 1;
+    num_equal = 0;
 
-function rightShift(not_equal, Arr_1, Arr_2){
-    rightArr_1 = Arr_1;
-    while(not_equal){
-        rightArr_1.push(rightArr_1.shift());
-        counter_r += 1;
-        for(var i = 0; i<rightArr_1.length; i++){
-            if(rightArr_1[i] != Arr_2[i]){
-                not_equal = true;
-                continue;
-            }else{
-                not_equal = false;
-            }
+    for(var i = 0; i < len_1; i++){
+        if (arr_r[i] == arr_2[i]){
+            num_equal += 1;
         }
     }
-    return counter_r;
-} 
+   
+    if (num_equal == len_1){
+        not_equal_right = false;
+    }
 
-//counter_l = leftShift(not_equal, Arr_1, Arr_2);
-counter_r = rightShift(not_equal, Arr_1, Arr_2);
+    if(counter_r == len_1){
+        counter = -1;
+        break;
+    }
+}
 
+if(counter == 0){
+    console.log(counter);
+}
 
+if(counter == -1){
+    console.log(counter);
+}
 
-
-
-console.log('----------------');
-console.log(Arr_1);
-console.log(Arr_2);
-//console.log(leftArr_1);
-//console.log(rightArr_1);
-console.log('counter_r = ' + counter_r);
-console.log('counter_l = ' + counter_l);
-console.log(not_equal);
+if (counter == -2){
+    if(counter_l <= counter_r){
+        console.log(counter_l);
+    }else{
+        console.log(counter_r);
+    }
+}
