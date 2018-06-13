93100f81f0122a48793ef22c83c9f0174c34fe72
Aplake <aplake@mail.ru>
2018-06-13 18:18:39 +0300

    Рабочая версия

--- task3.js
+++ task3.js
@@ -15,10 +15,64 @@ hh = (alltime/3600)^0;
 mm = ((alltime - hh*3600)/60)^0;
 ss = alltime - (hh*3600 + mm *60)
 
-if (hh != 0){
-    process.stdout.write(hh +" часов "+ mm + " минут " + ss + " секунд"); 
-}else if (mm === 0) {
-    process.stdout.write(ss + " секунд")
-} else {
-    process.stdout.write(mm + " минут " + ss + " секунд"); 
+hours   = ['час', 'часа', 'часов'];
+minuts  = ['минута', 'минуты', 'минут'];
+seconds = ['секнда', 'скунды', 'секунд'];
+
+function getEnding(number, endings)
+{
+    var outEndings, i;
+    number = number % 100;
+    if (number>=11 && number<=19) {
+        outEndings=endings[2];
+    }
+    else {
+        i = number % 10;
+        switch (i)
+        {
+            case (1): outEndings = endings[0]; break;
+            case (2):
+            case (3):
+            case (4): outEndings = endings[1]; break;
+            default: outEndings = endings[2];
+        }
+    }
+    return outEndings;
+}
+
+
+var hhEnds = getEnding(hh, hours);
+var mmEnds = getEnding(mm, minuts);
+var ssEnds = getEnding(ss, seconds);
+
+outHours = [hh, hhEnds].join(' ');
+outMinuts = [mm, mmEnds].join(' ');
+outSeconds = [ss, ssEnds].join(' ');
+
+if (hh !=0 && mm != 0 && ss !=0){
+    console.log(outHours, outMinuts, outSeconds);
+}
+
+if (hh != 0 && mm != 0 && ss == 0){
+    console.log(outHours, outMinuts);
+}
+
+if (hh != 0 && mm == 0 && ss != 0){
+    console.log(outHours, outSeconds);
+}
+
+if (hh != 0 && mm == 0 && ss == 0){
+    console.log(outHours);
+}
+
+if (hh == 0 && mm != 0 && ss != 0){
+    console.log(outMinuts, outSeconds);
+}
+
+if (hh == 0 && mm != 0 && ss == 0){
+    console.log(outMinuts);
+}
+
+if (hh == 0 && mm == 0 && ss != 0){
+    console.log(outSeconds);
 }
