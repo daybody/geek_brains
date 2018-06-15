d80b8cce2d36e360d4f9de15de6d332cc29d6b1b
Aplake <aplake@mail.ru>
2018-06-15 12:23:59 +0300

    Исправлена проблема с удалением пробелов из строки

--- task1.js
+++ task1.js
@@ -2,6 +2,6 @@ var nodePath = process.argv[0];
 var appPath = process.argv[1];
 var testStr = process.argv[2];
 
-var myStr =testStr.replace(/[К к Р р Н н]/g, '');
+var myStr =testStr.replace(/[КкРрНн]/g, '');
 
 process.stdout.write(myStr);
